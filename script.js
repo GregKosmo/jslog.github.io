ed847ed8bd39c478bce55ef1fe7bd67ceb453428
Greg <greg@gregkosmacek.com>
2019-12-20 01:22:44 -0600

    Pass all lighthouse tests

--- script.js
+++ script.js
@@ -53,3 +53,15 @@ function toggleDarkMode() {
 if(window.localStorage.getItem(COLOR_MODE_CACHE_KEY) === COLOR_MODE_DARK) {
     toggleDarkMode();
 }
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
